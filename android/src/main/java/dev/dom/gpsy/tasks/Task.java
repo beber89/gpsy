564ef612bfe367b31fc7d009621e9041f8b45391
unknown <mahmoudbaibars@gmail.com>
2019-08-05 12:18:18 +0200

    repushing

---
+++ android/src/main/java/dev/dom/gpsy/tasks/Task.java
@@ -0,0 +1,33 @@
+package dev.dom.gpsy.tasks;
+
+import dev.dom.gpsy.OnCompletionListener;
+
+import java.util.UUID;
+
+public abstract class Task<TOptions> {
+    private final UUID mTaskID;
+    private final TaskContext<TOptions> mTaskContext;
+
+    Task(TaskContext<TOptions> context) {
+        mTaskID = UUID.randomUUID();
+        mTaskContext = context;
+    }
+
+    public UUID getTaskID() {
+        return mTaskID;
+    }
+
+    TaskContext<TOptions> getTaskContext() {
+        return mTaskContext;
+    }
+
+    public abstract void startTask();
+
+    public void stopTask() {
+        OnCompletionListener completionListener = getTaskContext().getCompletionListener();
+
+        if(completionListener != null) {
+            completionListener.onCompletion(getTaskID());
+        }
+    }
+}
